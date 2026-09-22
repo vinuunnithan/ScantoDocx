5f04648cf67cf16f016cdc0ee9cf7a83ba80d819
vinuunnithan <101428629+vinuunnithan@users.noreply.github.com>
2026-09-22 00:15:56 -0500

    Update ScantoDocx.py

--- ScantoDocx.py
+++ ScantoDocx.py
@@ -42,7 +42,9 @@ with st.sidebar:
     # Track if the disclaimer has been signed
     is_signed = len(user_signature) > 0
 
-st.title("<h1 style='font-size: 32px;'> 📝 PNG/PDF→Markdown→Word Converter")
+#st.title("<h1 style='font-size: 32px;'> 📝 PNG/PDF→Markdown→Word Converter")
+
+st.markdown("<h1 style='font-size: 32px;'>📝 PNG/PDF→Markdown→Word Converter</h1>", unsafe_allow_html=True)
 st.write("Upload your PNG images or PDF files, convert them to clean Markdown via Gemini AI, and download an editable Word Document. App developed by Dr. Vinu Unnikrishnan in collaboration with **Gemini AI**.")
 
 # Check signature status before displaying instructions or core application tools
